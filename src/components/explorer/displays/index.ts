6076df18d8feb3a3589fc361cdfe883937f22e89
Abdullah Sikder <abdullahsikder@hotmail.com>
2020-08-19 12:53:09 +1000

    first commit

--- src/components/explorer/displays/index.ts
+++
@@ -1,6 +0,0 @@
-// Place all the displays here for easy export
-export  { default as CatchallDisplay} from "./CatchallDisplay";
-export { default as JsonDisplay} from "./JsonDisplay";
-export { default as IframeDisplay} from "./IframeDisplay";
-export { default as ImageDisplay} from "./ImageDisplay";
-export { default as DcmDisplay} from "./DcmDisplay";
